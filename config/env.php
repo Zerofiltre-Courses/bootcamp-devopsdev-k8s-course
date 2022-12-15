c81769e7c5d8454d15701ac91bae04a5ab40c7d4
willbrid <ngaswilly77@gmail.com>
2022-12-15 06:45:13 +0100

    feat: add chapitre 6

---
+++ config/env.php
@@ -0,0 +1,14 @@
+<?php
+
+return array (
+  'database_host' => 'mariadb-svc',
+  'database_port' => 3306,
+  'database_name' => 'solidinvoice',
+  'database_user' => 'solidinvoice',
+  'database_password' => 'SecretSolidinvoice',
+  'database_driver' => 'pdo_mysql',
+  'database_version' => '5.5.5-10.7.7-MariaDB-1:10.7.7+maria~ubu2004',
+  'locale' => 'fr_FR',
+  'installed' => '2022-12-15T04:37:56+01:00',
+  'secret' => 'def00000c27dd44a0d140e7eb3b6abee51a6550769ac5c41f5af99d54b1e7e62b3c761d601f4826b4e3f312e1319479bf808402f0f81c9970c57977e2e939193521e4bdc',
+);
